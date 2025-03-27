7bed34083d5c3e6a154de89588344817053248dc
Esraa Mohamed Hussein <esraa.hussein.93.eh@gmail.com>
2025-03-27 14:58:08 +0200

    utility

---
+++ EmailSender.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net.Mail;
+using System.Net;
+
+namespace New_Eticket.Utility
+{
+    public class EmailSender : IEmailSender
+
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(userName: "esraa.hussein.93.eh@gmail.com", password: "lrem kbmp tfmo hvrv")
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: "esraa.hussein.93.eh@gmail.com",
+                                to: email,
+                                subject,
+                                message
+                                )
+                {
+                    IsBodyHtml = true
+                });
+        }
+    }
+}
